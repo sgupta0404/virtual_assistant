d0ad214e579c3b53ed20dd270ba29db23a69aec3
Sanchit <sanchit04004@gmail.com>
2024-11-10 23:51:01 +0530

    Updated response.py

--- virtual_assist/weather_api.py
+++ virtual_assist/weather_api.py
@@ -7,7 +7,7 @@ from response import speak
 def get_weather(city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}"
     response = requests.get(url).json()
-    if response["cod"] == 200:
+    if response["cod"] == 200: # 200=ok
         temp = response["main"]["temp"] - 273.15  # Kelvin to Celsius
         description = response["weather"][0]["description"]
         speak(f"The current temperature is {temp:.2f}°C with {description}.")
